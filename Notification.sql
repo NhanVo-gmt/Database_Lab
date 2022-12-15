46c567980accf48346e01500115b456c57072868
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2022-12-16 00:35:58 +0700

    update notification

--- Notification.sql
+++ Notification.sql
@@ -5,6 +5,8 @@ in in_customer_id int, in in_isbn bigint
 )
 begin
 	insert into notification(Status, Customer_ID, ISBN) values (false, in_customer_id, in_isbn);
+    
+    call update_noti(in_isbn);
 end //
 
 DELIMITER ;
@@ -14,7 +16,19 @@ create procedure update_noti(
 in in_isbn bigint
 )
 begin
-	update notification set Status = true where Status = false and ISBN = in_isbn limit 1;
+	declare number_of_book_now int;
+    select number_of_book_remaining into number_of_book_now from book_remainder br where br.isbn = in_isbn;
+
+	if number_of_book_now > 0 then 
+		update notification set Status = true where Status = false and ISBN = in_isbn limit 1;
+    end if;
 end //
 
 DELIMITER ;
+
+-- call procedure example
+call create_noti(2053163,9783010907299);
+call create_noti(2053135,9789974344760);
+call create_noti(2053290,9782113180045);
+
+select * from notification
