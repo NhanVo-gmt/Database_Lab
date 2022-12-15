fc80838551677a4783c54f41cbd9c1e06dc1fd89
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2022-12-16 00:07:30 +0700

    update

---
+++ InsertFuncTable.sql
@@ -0,0 +1,13 @@
+-- Borrow Home Bill
+call create_borrowhome_bill(2, 2051535, 9781638257945);
+call create_borrowhome_bill(1, 2052580, 9787830516379);
+call create_borrowhome_bill(3, 2052858, 9788606352854);
+call create_borrowhome_bill(2, 2053642, 9785457731310);
+call create_borrowhome_bill(3, 2052858, 9785151051393);
+
+-- Read in library Bill
+call create_readinlibrary_bill(1, 2051535, 9786509130490);
+call create_readinlibrary_bill(2, 2053213, 9788667995035);
+call create_readinlibrary_bill(1, 2053642, 9788264825757);
+call create_readinlibrary_bill(2, 2053213, 9783010907299);
+call create_readinlibrary_bill(3, 2053290, 9783010907299);
